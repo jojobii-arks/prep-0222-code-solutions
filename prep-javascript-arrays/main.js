bf1600a6fd8479c43c8422a591a89cc2a85fa936
Joseph Bravo <josephangelobravo@gmail.com>
2022-03-01 15:32:55 +0000

    Completed prep-javascript-arrays assignment.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,14 @@
+var languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log('languages array ', languages);
+
+var firstElement = languages[0];
+console.log('first element: ', firstElement);
+
+var thirdElement = languages[2];
+console.log('third element: ', thirdElement);
+
+var length = languages.length;
+console.log('length: ', length);
+
+var lastElement = languages[languages.length - 1];
+console.log('last element: ', lastElement);
